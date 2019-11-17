a61688093ed85c27fe6ed20f2be14d4ec897da68
hamid <hamid331994@gmail.com>
2019-11-17 15:27:36 +0330

    Dev

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,45 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
 // import Context from '../../context/Context.js';
 
 const About = () => {
-  const page = useRef(null)
-
-  useEffect(() => {
-    console.log(page.current.getBoundingClientRect().top)
-    console.log(window.innerHeight)
-    const top = page.current.getBoundingClientRect().top;
-    const height = window.innerHeight;
-    if(top / height < 0.5 && top > 0) {
-      // page.current.style.color = 'blue'
-      // console.log(height / top)
-      page.current.classList.add('come')
-    }
-    // eslint-disable-next-line
-  }, [])
-
-  const onScroll = () => {
-    const top = page.current.getBoundingClientRect().top;
-    const height = window.innerHeight;
-    if(top / height < 0.5 && top > 0) {
-      // page.current.style.color = 'blue'
-      // console.log(height / top)
-      page.current.classList.add('come')
-      page.current.classList.remove('down')
-    } else if(top / height > 0.5) {
-      page.current.classList.remove('come')
-      page.current.classList.add('down')
-    }
-  }
-
-  const onClick = () => {
-
-  }
-
-  window.addEventListener('scroll', onScroll)
-
   return (
-    <div ref={page} className='page-container' onClick={onClick} onScroll={onScroll} id='about'>
+    <div className='page-container' id='about'>
       <div className='page'>
         AB
       </div>
