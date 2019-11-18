4ecacc55d6436b7db635fd72e99b649e7ffc383a
hamid <hamid331994@gmail.com>
2019-11-18 22:49:37 +0330

    dev

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import ReactDOM from 'react-dom';
 
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -19,25 +19,56 @@ import './styles/zIndex.css';
 import './components/pages/pages.css';
 
 const App = () => {
-  const {setActivePage} = useContext(Context)
+  const { activePage, setActivePage } = useContext(Context)
+
+  const [scNum, setScNum] = useState(0)
 
   const onScroll = () => {
+    // const scrollPercent = Math.floor(window.scrollY / (document.body.getBoundingClientRect().height - window.innerHeight) * 100)
+
+    // if(scrollPercent > 0 && scrollPercent <= 25) {
+    //   setActivePage('home')
+    // } else if(scrollPercent > 25 && scrollPercent <= 50) {
+    //   setActivePage('about')
+    // } else if(scrollPercent > 50 && scrollPercent <= 75) {
+    //   setActivePage('works')
+    // } else if(scrollPercent > 75 && scrollPercent <= 100) {
+    //   setActivePage('contact')
+    // }
+
+    // if(window.scrollY > scNum) {
+    //   if(activePage === 'home') {
+    //     setActivePage('about')
+    //   }
+    //   if(activePage === 'about') {
+    //     setActivePage('works')
+    //   }
+    //   if(activePage === 'works') {
+    //     setActivePage('contact')
+    //   }
+    //   setScNum(window.scrollY)
+    // } else if(window.scrollY < scNum) {
+    //   if(activePage === 'contact') {
+    //     setActivePage('works')
+    //   }
+    //   if(activePage === 'works') {
+    //     setActivePage('about')
+    //   }
+    //   if(activePage === 'about') {
+    //     setActivePage('home')
+    //   }
+    //   setScNum(window.scrollY)
+    // }
+
     console.clear();
-    console.log(window.scrollY);
-    console.log(window.innerHeight);
-    console.log((window.scrollY + window.innerHeight));
-    console.log(document.body.getBoundingClientRect().height);
+    clearTimeout(myT);
+    const myT = setTimeout(() => {
+      console.log(1000)
+    }, 1000);
 
-    const scrollPercent = Math.floor(window.scrollY / (document.body.getBoundingClientRect().height - window.innerHeight) * 100)
-
-    if((window.scrollY + window.innerHeight) === document.body.getBoundingClientRect().height) {
-      setActivePage('works')
-    } else if(true) {
-
-    }
   }
 
-  window.addEventListener('scroll', onScroll)
+  window.addEventListener('click', onScroll)
   
   return (
     // <>
