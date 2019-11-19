71c4adb40430ea0eb244ce32673adc057854778e
hamid <hamid331994@gmail.com>
2019-11-19 21:59:56 +0330

    dev

--- src/context/State.js
+++ src/context/State.js
@@ -5,10 +5,10 @@ import Reducer from './Reducer';
 const State = props => {
   const initialState = {
     myName: 'H',
-    activePage: 'about', // values: home, about, works, contact
-    pagesStatus: 'zoom-in',
-    pagesPos: 'top-right', // t-l | t-r | b-l | b-r
-    aPageIsActive: true, // sets too true After 1s for anims
+    activePage: 'home', // values: home, about, works, contact
+    pagesStatus: 'zoom-out',
+    pagesPos: 'top-left', // t-l | t-r | b-l | b-r
+    aPageIsActive: false, // sets too true After 1s for anims
   };
 
   const [state, dispatch] = useReducer(Reducer, initialState);
