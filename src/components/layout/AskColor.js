f73f88d850bb702e400ea69a5ffa8d0be15b15ae
hamid <hamid331994@gmail.com>
2019-12-05 15:55:51 +0330

    dev

--- src/components/layout/AskColor.js
+++ src/components/layout/AskColor.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 const AskColor = () => {
   console.warn('ask for color or choose randomlly or select one myself')
+
+  document.body.style.setProperty('--color-one', '#549623')
+
   return (
     <></>
   )
