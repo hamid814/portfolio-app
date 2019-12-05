497b79423b0374428dd77c111c1d92b50011fb0e
hamid <hamid331994@gmail.com>
2019-12-05 16:45:20 +0330

    dev

--- src/components/layout/AskColor.js
+++ src/components/layout/AskColor.js
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+
+import './AskColor.css';
 
 const AskColor = () => {
   console.warn('ask for color or choose randomlly or select one myself')
 
-  document.body.style.setProperty('--color-one', '#549623')
+  const [degree, setDegree] = useState(120)
+
+  const circle = useRef(null)
+
+  useEffect(() => {
+    rotate()
+
+    // eslint-disable-next-line
+  }, [])
+
+  const rotate = () => {
+    if(degree === 360) {
+      setDegree(1)
+    } else if(degree < 360) {
+      const z = degree + 1
+      setDegree(z)
+      console.log(degree)
+    }
+
+    circle.current.style.transform = `rotateZ(${degree}deg)`
+
+    setTimeout(() => {
+      rotate()
+    }, 800);
+  }
+
+  // document.body.style.setProperty('--color-one', '#222')
+  // document.body.style.setProperty('--color-two', '#555')
+  // document.body.style.setProperty('--color-three', '#888')
+  // document.body.style.setProperty('--color-four', '#bbb')
 
   return (
-    <></>
+    <div id='select-color' ref={circle}>
+
+    </div>
   )
 }
 
