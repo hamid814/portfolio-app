78052d2c2db4fa415be95cf2158946604c17a7bb
hamid <hamid331994@gmail.com>
2019-12-06 23:58:22 +0330

    dev

--- src/components/layout/AskColor.js
+++ src/components/layout/AskColor.js
@@ -5,73 +5,92 @@ import './AskColor.css';
 const AskColor = () => {
   const initialColors = {
     pack1: {
-      active: true,
       colorOne: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgb(38, 38, 38)',
+        opacit: 'rgba(38, 38, 38, 0.9)',
+        title: '#56617c'
       },
       colorTwo: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgba(140, 133, 41)',
+        opacit: 'rgba(140, 133, 41, 0.9)',
+        title: '#8C8529'
       },
       colorThree: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgba(86, 97, 124)',
+        opacit: 'rgba(86, 97, 124, 0.9)',
+        title: '#6E361A'
       },
       colorFour: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgba(110, 54, 26)',
+        opacit: 'rgba(110, 54, 26, 0.9)',
+        title: '#262626'
       }
     },
     pack2: {
-      active: true,
       colorOne: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgb(210, 180, 140)',
+        opacit: 'rgba(210, 180, 140, 0.9)',
+        title: 'rgb(255, 99, 71)'
       },
       colorTwo: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgb(0, 128, 128)',
+        opacit: 'rgba(0, 128, 128, 0.9)',
+        title: 'rgb(0, 128, 128)'
       },
       colorThree: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgb(255, 99, 71)',
+        opacit: 'rgba(255, 99, 71, 0.9)',
+        title: 'rgb(216, 191, 216)'
       },
       colorFour: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgb(216, 191, 216)',
+        opacit: 'rgba(216, 191, 216, 0.9)',
+        title: 'rgb(210, 180, 140)'
       }
     },
     pack3: {
-      active: true,
       colorOne: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgb(30, 30, 30)',
+        opacit: 'rgba(30, 30, 30, 0.9)',
+        title: '#56617c'
       },
       colorTwo: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgb(150, 150, 150)',
+        opacit: 'rgba(150, 150, 150, 0.9)',
+        title: '#8C8529'
       },
       colorThree: {
-        color: '',
-        opacit: '',
-        title: ''
+        color: 'rgb(210, 210, 210)',
+        opacit: 'rgba(210, 210, 210, 0.9)',
+        title: '#6E361A'
       },
       colorFour: {
-        color: '',
-        opacit: '',
-        title: ''
-      }
+        color: 'rgb(90, 90, 90)',
+        opacit: 'rgba(90, 90, 90, 0.9)',
+        title: '#262626'
+      },
+    },
+    pack4: {
+      colorOne: {
+        color: 'rgb(30, 30, 30)',
+        opacit: 'rgba(30, 30, 30, 0.9)',
+        title: '#56617c'
+      },
+      colorTwo: {
+        color: 'rgb(150, 150, 150)',
+        opacit: 'rgba(150, 150, 150, 0.9)',
+        title: '#8C8529'
+      },
+      colorThree: {
+        color: 'rgb(210, 210, 210)',
+        opacit: 'rgba(210, 210, 210, 0.9)',
+        title: '#6E361A'
+      },
+      colorFour: {
+        color: 'rgb(90, 90, 90)',
+        opacit: 'rgba(90, 90, 90, 0.9)',
+        title: '#262626'
+      },
     }
   }
 
@@ -79,30 +98,31 @@ const AskColor = () => {
 
   const [colors, setColors] = useState(initialColors)
 
+  const [activePack, setActivePack] = useState(1)
+
   const circle = useRef(null)
 
   let testD = 0;
 
   useEffect(() => {
-    const rotate = () => {
-      if(testD === 360 && true) {
-        testD = 1
-      } else if(testD < 360) {
-        testD++
-      }
-      if(!open) {
-        circle.current.style.transform = `rotateZ(${testD}deg)`
-      }
-
-      setTimeout(() => {
-        rotate()
-      }, 20);
-    }
-
     rotate()
     // eslint-disable-next-line
   }, [])
 
+  const rotate = () => {
+    if(testD === 360 && true) {
+      testD = 1
+    } else if(testD < 360) {
+      testD++
+    }
+    if(!open) {
+      circle.current.style.transform = `rotateZ(${testD}deg)`
+    }
+
+    setTimeout(() => {
+      rotate()
+    }, 20);
+  }
 
   const onClick = () => {
     setOpen(!open)
@@ -115,24 +135,26 @@ const AskColor = () => {
 
 
   return (
-    <div
-      id='select-color'
-      className={`${open ? 'open' : 'close'}`}
-      ref={circle}
-      onClick={onClick}>
-      <div className='color-one'>
-        <div className='pack-one'>
-          
-        </div>      
-      </div>
-      <div className='color-two'>
-        <div className='pack-two'>
-        
+    <div id='select-color-container'>
+      <div
+        id='select-color'
+        className={`${open ? 'open' : 'close'}`}
+        ref={circle}
+        onClick={onClick}>
+        <div className='color-one'>
+          <div className='pack-one'>
+            
+          </div>      
         </div>
-      </div>
-      <div className='color-three'>
-        <div className='pack-three'>
+        <div className='color-two'>
+          <div className='pack-two'>
           
+          </div>
+        </div>
+        <div className='color-three'>
+          <div className='pack-three'>
+            
+          </div>
         </div>
       </div>
     </div>
