a0ec28964096d6536c282f614b76de207276a8f6
hamid <hamid331994@gmail.com>
2020-01-01 23:19:56 +0330

    dev

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -147,14 +147,14 @@ const Home = () => {
         <div>
           I'm { myName }
         </div>
-        <div className='background-image'>
+        {/* <div className='background-image'> */}
 
-        </div>
-        <div className='circle'>
-          <div className='dot'></div>
-        </div>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1500 1000'>
-            <path fill="#fff" d="M 100 185 L 160 185 L 230 255 L 245 240 L 275 270 L 265 280 L 365 380 L 495 250 L 525 280 L 395 410 L 435 450 L 635 250 L 615 230 L 645 200 L 665 220">
+        {/* </div> */}
+        {/* <div className='circle'> */}
+          {/* <div className='dot'></div> */}
+        {/* </div> */}
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='50 150 1500 1000'>
+            <path fill="#fff" d="M 100 185 L 160 185 L 230 255 L 240 245 L 270 275 L 260 285 L 390 415 L 540 265 L 570 295 L 420 445 L 480 505 L 715 270 L 680 235 L 710 205 L 745 240 L 770 215 L 800 245 L 775 270 L 875 370 L 1045 200 L 1075 230 L 905 400 L 1005 500 L 1320 185 L 1380 185 L 1035 530 L 1135 630 L 1305 460 L 1335 490 L 1265 560 L 1315 610 L 1500 425 L 1500 485 L 1440 545 L 1500 605 L 1500 665 L 1480 645 L 1310 815 L 1250 815 L 1450 615 L 1410 575 L 1345 640 L 1375 670 L 1345 700 L 1235 590 L 1165 660 L 1205 700 L 1175 730 L 745 300 L 630 415 L 745 530 L 785 490 L 815 520 L 775 560 L 985 770 L 955 800 L 745 590 L 520 815 L 460 815 L 560 715 L 475 630 L 505 600 L 590 685 L 685 590 L 570 475 L 385 660 L 485 760 L 455 790 L 225 560 L 30 815 L 0 815 L 0 785 L 345 440 L 375 470 L 315 530 L 415 630 L 495 550 L 275 330 L 0 605 L 0 545 L 245 300 Z">
 
             </path>
           </svg>
